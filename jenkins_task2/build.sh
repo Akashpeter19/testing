c2dc6293a32204cf46825d83d1ab0693dd2e4041
Akash Peter P <akashpeter@Akashs-MacBook-Pro.local>
2026-02-12 14:40:59 +0530

    Added jenkins_task2 with build.sh for Task-2

---
+++ jenkins_task2/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "==============================="
+echo "Jenkins Task-2 Build Execution"
+echo "Triggered on: $(date)"
+echo "Build executed successfully!"
+echo "==============================="
+
